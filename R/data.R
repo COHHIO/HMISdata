50dd2238e04c7a9d41ec81b08d9623d7de760318
Trevin Flickinger <twflick@gmail.com>
2025-03-05 13:20:34 -0500

    add ServiceAreas to data

--- R/data.R
+++ R/data.R
@@ -11,3 +11,16 @@
 #' }
 #' @source Ohio Balance of State Continuum of Care
 "Regions"
+
+#' Ohio Homeless Service Areas
+#'
+#' A dataset containing the mapping of Ohio counties to their respective
+#' SSVF service
+#'
+#' @format A data frame with 88 rows and 2 variables:
+#' \describe{
+#'   \item{ssvf_service_area}{Name of the SSVF service}
+#'   \item{county}{County Name}
+#' }
+#' @source Ohio Balance of State Continuum of Care
+"ServiceAreas"
